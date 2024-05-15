cedf13d504be6c776c6db468deeb8d32e3484a7d
unknown <marcela2604ousa@gmail.com>
2024-05-15 16:22:08 -0300

    Primeiro commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use app\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index'])->name("home");
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
