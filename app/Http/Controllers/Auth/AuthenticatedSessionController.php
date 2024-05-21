7a04408cb2e2fe600eb01b7e706241e6d03dd49e
MarcelaSousa26 <marcela2604sousa@gmail.com>
2024-05-21 17:02:22 -0300

    04 commit

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -24,11 +24,18 @@ class AuthenticatedSessionController extends Controller
      */
     public function store(LoginRequest $request): RedirectResponse
     {
+        
         $request->authenticate();
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        if ($request->user()->usertype == 'admin'){
+            return redirect('admin/dashboard');
+        }
+
+        //return redirect()->intended(route('dashboard', absolute: false));
+        return redirect()->intended(route('dashboard'));
+
     }
 
     /**
